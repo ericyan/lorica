faa0cf394a939f01bd0a27b64f57517dfb25cedc
Eric Yan <github@ericyan.me>
2017-04-14 23:02:16 +0800

    Implement RSA key pair generation

--- token.go
+++ token.go
@@ -1,7 +1,12 @@
 package lorica
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"errors"
 	"fmt"
+	"hash/crc64"
+	"math/big"
 
 	"github.com/miekg/pkcs11"
 )
@@ -106,4 +111,95 @@ func (tk *Token) Info() (pkcs11.TokenInfo, error) {
 	}
 
 	return tokenInfo, nil
+}
+
+// Generate an RSA key pair of the given bit size inside the token.
+func (tk *Token) generateRSAKeyPair(label string, bits int) (pkcs11.ObjectHandle, pkcs11.ObjectHandle, error) {
+	keyID := uint(crc64.Checksum([]byte(label), crc64.MakeTable(crc64.ECMA)))
+
+	mechanism := []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS_KEY_PAIR_GEN, nil)}
+	publicKeyTemplate := []*pkcs11.Attribute{
+		// Common storage object attributes (PKCS #11-B 10.4)
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),
+		pkcs11.NewAttribute(pkcs11.CKA_PRIVATE, false),
+		pkcs11.NewAttribute(pkcs11.CKA_MODIFIABLE, false),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, label),
+		// Common key attributes (PKCS #11-B 10.7)
+		pkcs11.NewAttribute(pkcs11.CKA_ID, keyID),
+		// Common public key attributes (PKCS #11-B 10.8)
+		pkcs11.NewAttribute(pkcs11.CKA_ENCRYPT, true),
+		pkcs11.NewAttribute(pkcs11.CKA_VERIFY, true),
+		// RSA public key object attributes (PKCS #11-M1 6.1.2)
+		pkcs11.NewAttribute(pkcs11.CKA_MODULUS_BITS, bits),
+		pkcs11.NewAttribute(pkcs11.CKA_PUBLIC_EXPONENT, []byte{1, 0, 1}),
+	}
+	privateKeyTemplate := []*pkcs11.Attribute{
+		// Common storage object attributes (PKCS #11-B 10.4)
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),
+		pkcs11.NewAttribute(pkcs11.CKA_PRIVATE, true),
+		pkcs11.NewAttribute(pkcs11.CKA_MODIFIABLE, false),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, label),
+		// Common key attributes (PKCS #11-B 10.7)
+		pkcs11.NewAttribute(pkcs11.CKA_ID, keyID),
+		// Common private key attributes (PKCS #11-B 10.9)
+		pkcs11.NewAttribute(pkcs11.CKA_SENSITIVE, true),
+		pkcs11.NewAttribute(pkcs11.CKA_DECRYPT, true),
+		pkcs11.NewAttribute(pkcs11.CKA_SIGN, true),
+	}
+
+	return tk.module.GenerateKeyPair(tk.session, mechanism, publicKeyTemplate, privateKeyTemplate)
+}
+
+// Get the RSA public key using the object handle.
+func (tk *Token) getRSAPublicKey(handle pkcs11.ObjectHandle) (crypto.PublicKey, error) {
+	template := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_MODULUS, nil),
+		pkcs11.NewAttribute(pkcs11.CKA_PUBLIC_EXPONENT, nil),
+	}
+	attrs, err := tk.module.GetAttributeValue(tk.session, handle, template)
+	if err != nil {
+		return nil, err
+	}
+
+	n := big.NewInt(0)
+	e := int(0)
+	gotModulus, gotExponent := false, false
+	for _, a := range attrs {
+		switch a.Type {
+		case pkcs11.CKA_MODULUS:
+			n.SetBytes(a.Value)
+			gotModulus = true
+		case pkcs11.CKA_PUBLIC_EXPONENT:
+			bigE := big.NewInt(0)
+			bigE.SetBytes(a.Value)
+			e = int(bigE.Int64())
+			gotExponent = true
+		}
+	}
+	if !gotModulus {
+		return nil, errors.New("missing public modulus")
+	}
+	if !gotExponent {
+		return nil, errors.New("missing public exponent")
+	}
+
+	return &rsa.PublicKey{N: n, E: e}, nil
+}
+
+// GenerateKeyPair generates a key pair inside the token. For obvious
+// reasons, only the public key will be returned.
+//
+// TODO: Support elliptic curve key pair generation.
+func (tk *Token) GenerateKeyPair(label, algo string, size int) (crypto.PublicKey, error) {
+	switch algo {
+	case "rsa":
+		handle, _, err := tk.generateRSAKeyPair(label, size)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate key pair: %s", err)
+		}
+
+		return tk.getRSAPublicKey(handle)
+	default:
+		return nil, fmt.Errorf("unsupported algorithm: %s", algo)
+	}
 }
