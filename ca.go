61b7e7fbd081d160479fb9558144d1f3957f0247
Eric Yan <github@ericyan.me>
2017-04-24 22:12:57 +0800

    Provide default CA policy

--- ca.go
+++ ca.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/cloudflare/cfssl/config"
+	"github.com/cloudflare/cfssl/helpers"
 	"github.com/cloudflare/cfssl/signer"
 	"github.com/cloudflare/cfssl/signer/local"
 	"github.com/ericyan/lorica/cryptoki"
@@ -31,7 +32,19 @@ func (ca *CA) Sign(csrPEM []byte) ([]byte, error) {
 }
 
 // NewCA returns a new CA. If the CA does not have a certificate yet,
-// set cert to nil.
+// set cert to nil. The default CA policy will be used if policy is set
+// to nil.
 func NewCA(cert *x509.Certificate, policy *config.Signing, key *cryptoki.KeyPair) (*CA, error) {
+	if policy == nil {
+		policy = &config.Signing{
+			Default: &config.SigningProfile{
+				Usage:        []string{"cert sign", "crl sign"},
+				ExpiryString: "43800h",
+				Expiry:       5 * helpers.OneYear,
+				CAConstraint: config.CAConstraint{IsCA: true},
+			},
+		}
+	}
+
 	return &CA{cert, policy, key}, nil
 }
