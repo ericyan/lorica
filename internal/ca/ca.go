f25082c8a3b3a0042385d1ce66e1d23b1ecb9e68
Eric Yan <github@ericyan.me>
2017-12-15 21:27:50 +0800

    Reuse the signer for the lifetime of the CA

--- internal/ca/ca.go
+++ internal/ca/ca.go
@@ -4,7 +4,6 @@ import (
 	"crypto/x509"
 	"fmt"
 
-	"github.com/cloudflare/cfssl/config"
 	"github.com/cloudflare/cfssl/signer"
 	"github.com/cloudflare/cfssl/signer/local"
 	"github.com/ericyan/lorica/cryptoki"
@@ -12,32 +11,30 @@ import (
 
 // CA represents a certification authority.
 type CA struct {
-	Cert   *x509.Certificate
-	Policy *config.Signing
 	key    *cryptoki.KeyPair
+	signer *local.Signer
 }
 
 // Sign signs CSR and returns certificate. Both CSR and certificate are
 // PEM-encoded.
 func (ca *CA) Sign(csrPEM []byte) ([]byte, error) {
-	s, err := local.NewSigner(ca.key, ca.Cert, signer.DefaultSigAlgo(ca.key), ca.Policy)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create signer: %s", err)
-	}
-
 	req := signer.SignRequest{Request: string(csrPEM)}
 
-	return s.Sign(req)
+	return ca.signer.Sign(req)
 }
 
 // New returns a new CA. If the CA does not have a certificate yet,
-// set cert to nil. The default CA policy will be used if policy is set
-// to nil.
+// set cert to nil.
 func New(cert *x509.Certificate, cfg *Config, key *cryptoki.KeyPair) (*CA, error) {
-	signing, err := cfg.Signing()
+	policy, err := cfg.Signing()
 	if err != nil {
 		return nil, err
 	}
 
-	return &CA{cert, signing, key}, nil
+	signer, err := local.NewSigner(key, cert, signer.DefaultSigAlgo(key), policy)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create signer: %s", err)
+	}
+
+	return &CA{key, signer}, nil
 }
