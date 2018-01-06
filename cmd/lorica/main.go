2d1ac7d6fc2afb27d75d8b26962e7e39e6b1ee13
Eric Yan <github@ericyan.me>
2018-01-06 10:31:24 +0800

    Refactor CLI implementation

--- cmd/lorica/main.go
+++ cmd/lorica/main.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/cloudflare/cfssl/log"
+	"github.com/ericyan/lorica/cmd"
+	"github.com/ericyan/lorica/cryptoki"
 	"github.com/ericyan/lorica/internal/ca"
+	"github.com/ericyan/lorica/internal/procedure"
 	"github.com/joho/godotenv"
 )
 
@@ -32,22 +36,14 @@ func init() {
 }
 
 func main() {
-	commands := map[string]func([]string){
-		"info": infoCommand,
-		"init": initCommand,
-		"sign": signCommand,
-	}
-
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage:\n\tlorica command [-flags] arguments\n")
 		os.Exit(2)
 	}
+	command := os.Args[1]
 
-	cmd, ok := commands[os.Args[1]]
-	if !ok {
-		fmt.Printf("invalid command: %s\n", os.Args[1])
-		os.Exit(2)
-	}
+	flags.Parse(os.Args[2:])
+	args := flags.Args()
 
 	godotenv.Load()
 	opts.module = os.Getenv("LORICA_TOKEN_MODULE")
@@ -57,8 +53,6 @@ func main() {
 		fmt.Printf("missing token env\n")
 		os.Exit(2)
 	}
-
-	flags.Parse(os.Args[2:])
 
 	if opts.verbose {
 		log.Level = log.LevelDebug
@@ -79,5 +73,64 @@ func main() {
 		}
 	}
 
-	cmd(flags.Args())
+	// FIXME: Only init command requires a R/W session.
+	tk, err := cryptoki.OpenToken(opts.module, opts.label, opts.pin, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tk.Close()
+
+	switch command {
+	case "info":
+		info, err := tk.Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Token label:\t%s\n", info.Label)
+		fmt.Printf("Manufacturer:\t%s\n", info.ManufacturerID)
+		fmt.Printf("Token model:\t%s\n", info.Model)
+		fmt.Printf("Serial number:\t%s\n", info.SerialNumber)
+	case "init":
+		pem, err := procedure.Init(tk, cfg, opts.selfsign)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var outputFilename string
+		if opts.selfsign {
+			outputFilename = strings.Replace(opts.config, ".json", ".crt.pem", 1)
+		} else {
+			outputFilename = strings.Replace(opts.config, ".json", ".csr.pem", 1)
+		}
+		err = cmd.WriteFile(outputFilename, pem)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "sign":
+		caPEM, err := cmd.ReadFile(opts.ca)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		csrFilename := args[0]
+		csrPEM, err := cmd.ReadFile(csrFilename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		certPEM, err := procedure.Sign(tk, cfg, caPEM, csrPEM)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		certPEMFilename := strings.TrimSuffix(csrFilename, ".csr.pem") + ".crt.pem"
+		err = cmd.WriteFile(certPEMFilename, certPEM)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		fmt.Printf("invalid command: %s\n", command)
+		os.Exit(2)
+	}
 }
