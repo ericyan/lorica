b881d6cdea035f7620d49d45374e71d284b10661
Eric Yan <github@ericyan.me>
2018-06-04 22:13:07 +0800

    Add integration test for subordinate CA

--- pkg/ca/integration_test.go
+++ pkg/ca/integration_test.go
@@ -4,7 +4,6 @@ import (
 	"crypto"
 	"crypto/ecdsa"
 	"io/ioutil"
-	"log"
 	"os"
 	"testing"
 
@@ -24,6 +23,25 @@ func (kp *fakeKeyProvider) FindKeyPair(key crypto.PublicKey) (crypto.Signer, err
 	return helpers.ParsePrivateKeyPEM(kp.keyPEM)
 }
 
+func initCA(configFile string, kp ca.KeyProvider) (string, error) {
+	config, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return "", err
+	}
+
+	cfg, err := ca.LoadConfig(config)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = ca.Init(cfg, kp)
+	if err != nil {
+		return "", err
+	}
+
+	return cfg.CAFile, nil
+}
+
 func TestRootCA(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test in short mode")
@@ -35,36 +53,89 @@ func TestRootCA(t *testing.T) {
 	}
 	kp := &fakeKeyProvider{keyPEM}
 
-	config, err := ioutil.ReadFile("testdata/root_ca.json")
+	caFile, err := initCA("testdata/root_ca.json", kp)
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer os.Remove(caFile)
 
-	cfg, err := ca.LoadConfig(config)
+	rootCA, err := ca.Open(caFile, kp)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	_, err = ca.Init(cfg, kp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(cfg.CAFile)
-
-	rootCA, err := ca.Open(cfg.CAFile, kp)
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	caPub, err := rootCA.PublicKey()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	kpKey, _ := kp.FindKeyPair(caPub)
 
 	a := caPub.(*ecdsa.PublicKey)
 	b := kpKey.Public().(*ecdsa.PublicKey)
 	if a.Curve.Params().Name != b.Curve.Params().Name || a.X.String() != b.X.String() || a.Y.String() != b.Y.String() {
-		log.Fatal("ca public key mismatch")
+		t.Fatal("ca public key mismatch")
+	}
+}
+
+func TestSubordinateCA(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test in short mode")
+	}
+
+	keyPEM, err := helpers.ReadBytes("testdata/prime256v1.key")
+	if err != nil {
+		t.Fatal(err)
+	}
+	kp := &fakeKeyProvider{keyPEM}
+
+	rootCAFile, err := initCA("testdata/root_ca.json", kp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(rootCAFile)
+
+	subCAFile, err := initCA("testdata/subordinate_ca.json", kp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(subCAFile)
+
+	rootCA, err := ca.Open(rootCAFile, kp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	subCA, err := ca.Open(subCAFile, kp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	subCSR, err := subCA.CertificateRequestPEM()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	subCertPEM, err := rootCA.Issue(subCSR)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = subCA.ImportCertificate(subCertPEM)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	subCert, err := subCA.Certificate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rootCert, err := rootCA.Certificate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if subCert.Issuer.String() != rootCert.Subject.String() {
+		t.Error("subordinate ca cert not issued by root ca")
 	}
 }
