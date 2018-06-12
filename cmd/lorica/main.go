fae97ea567af7ac89ac6f5245185eae0e8622391
Eric Yan <github@ericyan.me>
2018-06-12 21:51:53 +0800

    Rewrite CLI with Kingpin

--- cmd/lorica/main.go
+++ cmd/lorica/main.go
@@ -1,12 +1,11 @@
-// A command line tool for operating a certification authority.
+// Package main implements a command-line tool, lorica, for operating a
+// certification authority.
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/cloudflare/cfssl/log"
@@ -14,71 +13,52 @@ import (
 	"github.com/ericyan/lorica/pkg/ca"
 	"github.com/ericyan/lorica/pkg/cryptoki"
 	"github.com/joho/godotenv"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var flags = flag.NewFlagSet("lorica", flag.ExitOnError)
-var opts struct {
-	module  string
-	label   string
-	pin     string
-	config  string
-	ca      string
-	verbose bool
-}
-var cfg *ca.Config
+var (
+	app     = kingpin.New("lorica", "A command-line tool for operating a certification authority.")
+	caFile  = app.Flag("ca", "path to CA database").String()
+	verbose = app.Flag("verbose", "increase verbosity").Bool()
 
-func init() {
-	flags.StringVar(&opts.config, "config", "", "path to configuration file")
-	flags.StringVar(&opts.ca, "ca", "", "database of the signing CA")
-	flags.BoolVar(&opts.verbose, "v", false, "increase verbosity")
-}
+	infoCmd = app.Command("info", "Show infomation about the PKCS #11 token.")
+
+	initCmd  = app.Command("init", "Initialize a certification authority.")
+	initCfg  = initCmd.Arg("config", "path to configuration file").Required().String()
+	initCSR  = initCmd.Flag("csr", "output filename for CA CSR").String()
+	initCert = initCmd.Flag("cert", "output filename for CA certificate").String()
+
+	issueCmd  = app.Command("issue", "Issue a certificate.")
+	issueCSR  = issueCmd.Arg("csr", "path to CSR file (must be PEM-encoded)").Required().String()
+	issueCert = issueCmd.Flag("cert", "output filename for issued certificate").String()
+
+	revokeCmd    = app.Command("revoke", "Revoke a certificate.")
+	revokeSerial = revokeCmd.Arg("serial", "serial number of the certificate to be reovked").Required().String()
+	revokeReason = revokeCmd.Arg("reason", "RFC 5280 reason code for revocation").Default("0").Int()
+
+	crlCmd  = app.Command("crl", "Generate a new Certificate Revocation List.")
+	crlFile = crlCmd.Arg("file", "output filename for CRL").Required().String()
+	crlTTL  = crlCmd.Flag("ttl", "number of days after which the CRL will expire").Default("30").Int()
+)
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage:\n\tlorica command [-flags] arguments\n")
-		os.Exit(2)
-	}
-	command := os.Args[1]
+	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	flags.Parse(os.Args[2:])
-	args := flags.Args()
-
-	godotenv.Load()
-	opts.module = os.Getenv("LORICA_TOKEN_MODULE")
-	opts.label = os.Getenv("LORICA_TOKEN_LABEL")
-	opts.pin = os.Getenv("LORICA_TOKEN_PIN")
-	if opts.module == "" || opts.label == "" || opts.pin == "" {
-		fmt.Printf("missing token env\n")
-		os.Exit(2)
-	}
-
-	if opts.verbose {
+	if *verbose {
 		log.Level = log.LevelDebug
 	} else {
 		log.Level = log.LevelInfo
 	}
 
-	if opts.config != "" {
-		log.Debugf("loading configuration file from %s", opts.config)
-		config, err := ioutil.ReadFile(opts.config)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		cfg, err = ca.LoadConfig(config)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	tk, err := cryptoki.OpenToken(opts.module, opts.label, opts.pin)
+	godotenv.Load()
+	tk, err := cryptoki.OpenToken(os.Getenv("LORICA_TOKEN_MODULE"), os.Getenv("LORICA_TOKEN_LABEL"), os.Getenv("LORICA_TOKEN_PIN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer tk.Close()
 
 	switch command {
-	case "info":
+	case infoCmd.FullCommand():
 		info, err := tk.Info()
 		if err != nil {
 			log.Fatal(err)
@@ -88,43 +68,55 @@ func main() {
 		fmt.Printf("Manufacturer:\t%s\n", info.ManufacturerID)
 		fmt.Printf("Token model:\t%s\n", info.Model)
 		fmt.Printf("Serial number:\t%s\n", info.SerialNumber)
-	case "init":
+	case initCmd.FullCommand():
+		config, err := ioutil.ReadFile(*initCfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfg, err := ca.LoadConfig(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		ca, err := ca.Init(cfg, tk)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		var pem []byte
-		var outputFilename string
+		if *initCSR != "" {
+			csr, err := ca.CertificateRequestPEM()
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		pem, err = ca.CertificatePEM()
-		if err != nil {
-			log.Error(err)
-		}
-
-		outputFilename = strings.Replace(opts.config, ".json", ".crt.pem", 1)
-
-		if pem == nil {
-			pem, err = ca.CertificateRequestPEM()
+			err = cliutil.WriteFile(*initCSR, csr)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
 
-		outputFilename = strings.Replace(opts.config, ".json", ".csr.pem", 1)
+		if *initCert != "" {
+			if !cfg.SelfSign {
+				log.Fatal("certificate available for self-signed CA only")
+			}
 
-		err = cliutil.WriteFile(outputFilename, pem)
+			cert, err := ca.CertificatePEM()
+			if err != nil {
+				log.Error(err)
+			}
+
+			err = cliutil.WriteFile(*initCert, cert)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	case issueCmd.FullCommand():
+		csrPEM, err := cliutil.ReadFile(*issueCSR)
 		if err != nil {
 			log.Fatal(err)
 		}
-	case "sign":
-		csrFilename := args[0]
-		csrPEM, err := cliutil.ReadFile(csrFilename)
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		ca, err := ca.Open(opts.ca, tk)
+		ca, err := ca.Open(*caFile, tk)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -134,13 +126,12 @@ func main() {
 			log.Fatal(err)
 		}
 
-		certPEMFilename := strings.TrimSuffix(csrFilename, ".csr.pem") + ".crt.pem"
-		err = cliutil.WriteFile(certPEMFilename, certPEM)
+		err = cliutil.WriteFile(*issueCert, certPEM)
 		if err != nil {
 			log.Fatal(err)
 		}
-	case "revoke":
-		ca, err := ca.Open(opts.ca, tk)
+	case revokeCmd.FullCommand():
+		ca, err := ca.Open(*caFile, tk)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -150,27 +141,24 @@ func main() {
 			log.Fatal(err)
 		}
 
-		err = ca.Revoke(args[0], string(keyID), 0)
+		err = ca.Revoke(*revokeSerial, string(keyID), *revokeReason)
 		if err != nil {
 			log.Fatal(err)
 		}
-	case "crl":
-		ca, err := ca.Open(opts.ca, tk)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		crlDER, err := ca.CRL(30 * 24 * time.Hour)
+	case crlCmd.FullCommand():
+		ca, err := ca.Open(*caFile, tk)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		err = cliutil.WriteFile("crl.der", crlDER)
+		crlDER, err := ca.CRL(time.Duration(*crlTTL) * 24 * time.Hour)
 		if err != nil {
 			log.Fatal(err)
 		}
-	default:
-		fmt.Printf("invalid command: %s\n", command)
-		os.Exit(2)
+
+		err = cliutil.WriteFile(*crlFile, crlDER)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
